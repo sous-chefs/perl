6e21abe0eb034fbdc594fc0437edaa2071366145
Lamont Granquist <lamont@opscode.com>
2012-03-27 12:40:06 -0700

    COOK-1129: centos6 path bug, plus fixing many platform issues, bumping version

--- metadata.rb
+++ metadata.rb
@@ -3,10 +3,10 @@ maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs perl and provides a define for maintaining CPAN modules"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.10.0"
+version           "0.10.1"
 
 recipe "perl", "Installs perl and provides a script to install cpan modules"
 
-%w{ ubuntu debian centos redhat arch}.each do |os|
+%w{ ubuntu debian mint redhat centos scientific oracle fedora arch}.each do |os|
   supports os
 end
