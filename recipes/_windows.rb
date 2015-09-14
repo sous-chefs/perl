83ffe0e0035fc8f40b4fc51f80ce6e030d4a4cce
Tim Smith <tsmith84@gmail.com>
2015-09-13 22:34:18 -0700

    Update copyright dates

--- recipes/_windows.rb
+++ recipes/_windows.rb
@@ -1,8 +1,8 @@
 #
 # Cookbook Name:: perl
-# Recipe:: default
+# Recipe:: _windows
 #
-# Copyright 2013 Chef
+# Copyright 2013-2015 Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
