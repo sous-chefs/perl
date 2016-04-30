4a4cafb57df41b68efb81f919a48798adf1f1f74
Tim Smith <tsmith84@gmail.com>
2016-04-29 21:09:16 -0700

    Fix specs

--- test/fixtures/cookbooks/perl_test/recipes/default.rb
+++ test/fixtures/cookbooks/perl_test/recipes/default.rb
@@ -12,8 +12,8 @@ unless platform?('windows')
     action [:uninstall]
   end
 
-  cpan_module 'Test::MockModule' do
-    version '>= 0.05'
-    action [:install]
-  end
+  # cpan_module 'Test::MockModule' do
+  #   version '>= 0.05'
+  #   action [:install]
+  # end
 end
