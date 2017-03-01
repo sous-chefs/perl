9c138bc86c46d56dff1949093b33545a1a3027f0
Tim Smith <tsmith@chef.io>
2017-03-01 11:03:28 -0800

    Update specs to match the Chef 13 fixes
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/unit/cpan_module_spec.rb
+++ spec/unit/cpan_module_spec.rb
@@ -7,9 +7,9 @@ describe 'perl_test::default' do
                             .converge(described_recipe)
     end
 
-    it 'expects cpan LWRP to run with install action' do
+    it 'expects cpan resource to run with install action' do
       stub_command("perl -mTest::MockModule -e ';' 2> /dev/null").and_return(false)
-      expect(chef_run).to install_cpan_module('Test::MockModule')
+      expect(chef_run).to install_cpan_module('Install test module')
     end
 
     it 'installs Test::MockModule via CPAN' do
@@ -17,9 +17,9 @@ describe 'perl_test::default' do
       expect(chef_run).to run_execute('CPAN :install Test::MockModule')
     end
 
-    it 'expects cpan LWRP to run with uninstall action' do
+    it 'expects cpan resource to run with uninstall action' do
       stub_command("perl -mTest::MockModule -e ';' 2> /dev/null").and_return(true)
-      expect(chef_run).to uninstall_cpan_module('Test::MockModule')
+      expect(chef_run).to uninstall_cpan_module('Uninstall test module')
     end
 
     it 'installs Test::MockModule via CPAN' do
